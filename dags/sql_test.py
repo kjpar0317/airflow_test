63597eec14d2165dbde3edf6fa8cbdf61ac439b3
박광주 <kjpar0317@innogrid.com>
2024-03-13 15:55:46 +0900

    minor수정

--- dags/sql_test.py
+++ dags/sql_test.py
@@ -1,6 +1,4 @@
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
 from airflow.sensors.sql import SqlSensor
 
 import logging
